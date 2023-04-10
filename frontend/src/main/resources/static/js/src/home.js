3805f050e91ff5a6286f0040a73e944274191e5d
CamaroTheBOSS <plessico@wp.pl>
2023-04-10 22:01:27 +0200

    Improved home page

--- frontend/src/main/resources/static/js/src/home.js
+++ frontend/src/main/resources/static/js/src/home.js
@@ -1,45 +1,3 @@
-function setAttributes(el, attrs) {
-  for(var key in attrs) {
-    el.setAttribute(key, attrs[key]);
-  }
-}
-
-function addSquareFrame(node, x, y, w, h, thickness = 2, padding = 2) {
-    const squareSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    setAttributes(squareSVG,
-    {'xmlns': 'http://www.w3.org/2000/svg',
-        'viewBox': [x, y, w, h].join(" "),
-        'width': w,
-        'height': h,
-        'thickness': thickness,
-        'padding': padding
-    });
-
-
-    pathway = `M${padding} ${padding} L${padding} ${h - padding} L${w - padding} ${h - padding} L${w - padding} ${padding} L${padding} ${padding}`
-    console.log(pathway);
-    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    setAttributes(path,
-    {'xmlns': 'http://www.w3.org/2000/svg',
-    'fill': 'none',
-    'stroke': 'black',
-    'stroke-width': thickness,
-    'd': pathway
-    });
-    var xd = "xd";
-    squareSVG.appendChild(path);
-    const div = document.createElement('div').appendChild(squareSVG);
-    return node.appendChild(div);
-}
-
-function addText(node, text) {
-  const div = document.createElement('div');
-  var textObj = document.createElement('p');
-  textObj.textContent = text;
-  div.appendChild(textObj);
-  return node.appendChild(div);
-}
-
 async function fetchDestinations(el) {
   await fetch('http://localhost:8080/destinations', {method: "GET"})
   .then(response => response.json())
@@ -48,8 +6,7 @@ async function fetchDestinations(el) {
     data.forEach(item => {
                 const destinationItem = document.createElement('div');
                 destinationItem.setAttribute("class", "destination_card");
-                addSquareFrame(destinationItem, 0, 0, 100, 100);
-                addText(destinationItem, item.name);
+                destinationItem.appendChild(squareFrame(0, 0, 100, 100, 2, 2, txt=item.name));
                 listOfDestinations.appendChild(destinationItem);
               });
     const container = document.getElementById('container');
