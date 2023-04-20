440d59f5e767df01fae2fc0348b9da8751d0de2d
Kacper <kacperkrzeminski123@gmail.com>
2023-04-20 18:35:42 +0200

    Change events back to classes, add events for offers. fix some inconsistencies between event and dto classes

--- Events/Flights/CreateFlightEvent.java
+++ Events/Flights/CreateFlightEvent.java
@@ -1,9 +1,25 @@
 package Events.Flights;
 
-public record CreateFlightEvent(String id, String airline_name, String departure_country,
-                                String departure_city, String date,
-                                String arrival_country, String arrival_city,
-                                Integer available_seats) {
+public class CreateFlightEvent {
+    private final String id;
+    private final String airline_name;
+    private final String departure_country;
+    private final String departure_city;
+    private final String date;
+    private final String arrival_country;
+    private final String arrival_city;
+    private final Integer available_seats;
+
+    public CreateFlightEvent(String id, String airline_name, String departure_country, String departure_city, String date, String arrival_country, String arrival_city, Integer available_seats) {
+        this.id = id;
+        this.airline_name = airline_name;
+        this.departure_country = departure_country;
+        this.departure_city = departure_city;
+        this.date = date;
+        this.arrival_country = arrival_country;
+        this.arrival_city = arrival_city;
+        this.available_seats = available_seats;
+    }
 
     public String getId() {
         return id;
--- Events/Offers/UpdateOfferEvent.java
+++ Events/Offers/UpdateOfferEvent.java
@@ -2,12 +2,42 @@ package Events.Offers;
 
 import java.util.Optional;
 
-public record UpdateOfferEvent(String id, String hotel_name, String image,
-                               String country, Integer stars, String start_date,
-                               String end_date, String room_type, Integer max_adults,
-                               Integer max_children_to_3, Integer max_children_to_10,
-                               Integer max_children_to_18, String meals, Double price,
-                               Boolean available) {
+public class UpdateOfferEvent{
+    private final String id;
+    private final String hotel_name;
+    private final String image;
+    private final String country;
+    private final String city;
+    private final Integer stars;
+    private final String start_date;
+    private final String end_date;
+    private final String room_type;
+    private final Integer max_adults;
+    private final Integer max_children_to_3;
+    private final Integer max_children_to_10;
+    private final Integer max_children_to_18;
+    private final String meals;
+    private final Double price;
+    private final Boolean available;
+
+    public UpdateOfferEvent(String id, String hotel_name, String image, String country, String city, Integer stars, String start_date, String end_date, String room_type, Integer max_adults, Integer max_children_to_3, Integer max_children_to_10, Integer max_children_to_18, String meals, Double price, Boolean available) {
+        this.id = id;
+        this.hotel_name = hotel_name;
+        this.image = image;
+        this.country = country;
+        this.city = city;
+        this.stars = stars;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.room_type = room_type;
+        this.max_adults = max_adults;
+        this.max_children_to_3 = max_children_to_3;
+        this.max_children_to_10 = max_children_to_10;
+        this.max_children_to_18 = max_children_to_18;
+        this.meals = meals;
+        this.price = price;
+        this.available = available;
+    }
 
     public String getId() {
         return id;
@@ -23,6 +53,10 @@ public record UpdateOfferEvent(String id, String hotel_name, String image,
 
     public Optional<String> getCountry() {
         return Optional.ofNullable(country);
+    }
+
+    public Optional<String> getCity() {
+        return Optional.ofNullable(city);
     }
 
     public Optional<Integer> getStars() {
