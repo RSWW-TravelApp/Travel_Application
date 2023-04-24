d4a16ff4afe1c65ba3e02a73592e1e91659cfd10
Kacper <kacperkrzeminski123@gmail.com>
2023-04-24 18:02:23 +0200

    fix reservation master name

--- reservation-master-service/src/main/java/reservationmaster/ReservationMasterApplication.java
+++ reservation-master-service/src/main/java/reservationmaster/ReservationMasterApplication.java
@@ -1,12 +1,12 @@
-package paymentmaster;
+package reservationmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymentMasterApplication {
+public class ReservationMasterApplication {
     public static void main(String[] args) {
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        SpringApplication.run(PaymentMasterApplication.class, args);
+        SpringApplication.run(ReservationMasterApplication.class, args);
     }
 }
