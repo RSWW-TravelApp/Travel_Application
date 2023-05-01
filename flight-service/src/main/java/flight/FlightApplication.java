6ab08415a8c51dcb199b585e42d20155f38c3f3b
CamaroTheBOSS <plessico@wp.pl>
2023-05-01 15:03:40 +0200

    Configured discovery server. Now offer service, flight service and api gateway are registered in discovery server. Api gateway is load balancing requests between all accessible services, which runs on random ports

--- flight-service/src/main/java/flight/FlightApplication.java
+++ flight-service/src/main/java/flight/FlightApplication.java
@@ -4,11 +4,13 @@ import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableReactiveMongoRepositories
 public class FlightApplication extends AbstractReactiveMongoConfiguration {
 
