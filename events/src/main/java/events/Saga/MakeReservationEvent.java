908c7bb68b4c71b8cb297d50257268405fa1f7e8
Kacper  Krzemiński <45371177+Kasperskyy@users.noreply.github.com>
2023-05-14 13:23:43 +0200

    Update MakeReservationEvent.java

--- events/src/main/java/events/Saga/MakeReservationEvent.java
+++ events/src/main/java/events/Saga/MakeReservationEvent.java
@@ -4,16 +4,6 @@ import java.io.Serializable;
 
 public class MakeReservationEvent extends SagaEvent {
 
-//    private final String room_type;
-//    private final Integer adult;
-//    private final Integer children_to_3;
-//    private final Integer children_to_10;
-//    private final Integer getChildren_to_18;
-//    private final String meals;
-//    private final Integer number_of_days;
-//    private final String start_date;
-//    private final String end_date;
-//    private final String discount;
     private final String is_paid;
 
 
@@ -22,45 +12,6 @@ public class MakeReservationEvent extends SagaEvent {
         this.is_paid = is_paid;
     }
 
-//    public String getRoom_type() {
-//        return room_type;
-//    }
-//
-//    public Integer getAdult() {
-//        return adult;
-//    }
-//
-//    public Integer getChildren_to_3() {
-//        return children_to_3;
-//    }
-//
-//    public Integer getChildren_to_10() {
-//        return children_to_10;
-//    }
-//
-//    public Integer getGetChildren_to_18() {
-//        return getChildren_to_18;
-//    }
-//
-//    public String getMeals() {
-//        return meals;
-//    }
-//
-//    public Integer getNumber_of_days() {
-//        return number_of_days;
-//    }
-//
-//    public String getStart_date() {
-//        return start_date;
-//    }
-//
-//    public String getEnd_date() {
-//        return end_date;
-//    }
-//
-//    public String getDiscount() {
-//        return discount;
-//    }
 
     public String isIs_paid() {
         return is_paid;
