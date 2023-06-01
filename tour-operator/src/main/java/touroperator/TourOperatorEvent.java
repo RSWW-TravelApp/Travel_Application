e9696ce0de0371a2efaecd31182eccb9454acbe2
Kacper <kacperkrzeminski123@gmail.com>
2023-06-01 18:16:51 +0200

    tour operator notifications init

--- tour-operator/src/main/java/touroperator/TourOperatorEvent.java
+++ tour-operator/src/main/java/touroperator/TourOperatorEvent.java
@@ -1,12 +1,16 @@
 package touroperator;
 
+import events.CQRS.flights.UpdateFlightEvent;
+import events.CQRS.offers.UpdateOfferEvent;
 import events.Saga.TONotificationEvent;
+import org.springframework.cloud.function.context.PollableBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Component
 public class TourOperatorEvent {
@@ -16,5 +20,15 @@ public class TourOperatorEvent {
         return flux -> flux
                 .doOnNext(event -> System.out.println("notified of purchase. I earned: " + event.getPrice() + " From offer id: " + event.getOfferId()+ " bought by user: " + event.getUser_id()))
                 .then();
+    }
+
+    @PollableBean
+    public Supplier<Flux<UpdateOfferEvent>> modifyOffer() {
+        return () -> Flux.just(new UpdateOfferEvent(null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null));
+    }
+
+    @PollableBean
+    public Supplier<Flux<UpdateFlightEvent>> modifyFLight() {
+        return () -> Flux.just(new UpdateFlightEvent(null,null,null,null,null,null,null));
     }
 }
